585cf97eceadb779d917818c536e029be947f0a0
xycodevip-coder <xycode.vip@gmail.com>
2026-03-05 18:43:49 +0200

    Setup GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/website/",
   server: {
     host: "::",
     port: 8080,
